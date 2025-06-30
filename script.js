fb7d8860368e2447bedced568eb5bb5623654c25
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-06-30 12:31:54 +0800

    Add ability to evaluate expressions

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function add(a, b) {
-    return a + b;
+    return parseFloat((a + b).toFixed(3)).toString();
 }
 
 function subtract(a, b) {
-    return a - b;
+    return parseFloat((a - b).toFixed(3)).toString();
 }
 
 function multiply(a, b) {
-    return a * b;
+    return parseFloat((a * b).toFixed(3)).toString();
 }
 
 function divide(a, b) {
-    return a / b;
+    return parseFloat((a / b).toFixed(3)).toString();
 }
 
 function operate(operator, a, b) {
@@ -27,13 +27,48 @@ function operate(operator, a, b) {
     }
 }
 
-function writeToDisplay(item) {
+let num1 = "";
+let num2 = "";
+let operator = "";
+const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const operators = ["+", "-", "×", "÷"];
+
+function writeToDisplay(item, equal=false) {
     const display = document.querySelector("#display");
-    display.textContent += item;
+    if (display.textContent.trim() === "" && (nums.includes(item) || item === "+" || item === "-")) {
+        num1 += item;
+        display.textContent += item;
+    } else if (num1 !== "" && operator === "") {
+        if (operators.includes(item)) {
+            operator = item;
+            display.textContent += item;
+        } else if (nums.includes(item)) {
+            num1 += item;
+            display.textContent += item;
+        }
+    } else {
+        if (nums.includes(item)) {
+            num2 += item;
+            display.textContent += item
+        } else if ((operators.includes(item) && num2 !== "") || equal) {
+            num1 = (operate(operator, Number(num1), Number(num2)));
+            num2 = "";
+            operator = item;
+            display.textContent = num1 + operator;
+        }
+    }
 }
+
+
 
 document.querySelectorAll(".num, .operator, #decimal").forEach(function(button) {
     button.addEventListener("click", function() {
         writeToDisplay(button.textContent);
     });
+});
+
+document.querySelector("#equal").addEventListener("click", function() {
+    if (num1 !== "" && num2 !== "" && operator !== "") {
+        writeToDisplay("", true);
+    }
 });
